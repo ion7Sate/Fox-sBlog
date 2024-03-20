cb0889ca244a2e06e0982c61b9c1a3d3b1c2c86d
ion7Sate <saptesate31@gmail.com>
2024-03-20 19:44:07 +0100

    Add a middleware  and a function to hondle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import {errorHandler} from "../utils/error.js";
 
-export const singup = async (req,res) => {
+export const singup = async (req,res,next) => {
     const { username, email ,password} = req.body;
 
     if(!username || !email || !password || username === ' ' || email === ' ' || password ===' '){
-        return res.status(400).json({message: 'All field are reqquired'});
+        next(errorHandler(400,'All field are required'));
     }
 
      const hashedpassword = bcryptjs.hashSync(password,10);
@@ -21,7 +22,7 @@ export const singup = async (req,res) => {
         await newUser.save();
         res.json('SignUp succesfuly');
     } catch (error){
-        res.status(500).json({message:error.message})
+        next(error);
     }
 
 }
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import {configDotenv} from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import res from "express/lib/response.js";
 
 configDotenv();
 mongoose
@@ -22,4 +23,14 @@ app.listen(3000,()=>{
 });
 
 app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoute)
+app.use('/api/auth',authRoute);
+
+app.use((err, req, res, next)=>{
+    const stausCode = err.statusCode || 500;
+    const message  = err.message || 'Internal Server Error';
+    res.status(stausCode).json({
+        succes : false ,
+            stausCode,
+            message
+    });
+});
